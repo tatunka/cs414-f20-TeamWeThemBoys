91c896e6e9e905a91d0dea09717ddad1a95d455e
wstnmssr <38819883+wstnmssr@users.noreply.github.com>
2020-11-09 19:28:53 -0700

    create profile view model

--- xGame/Java/xgame/src/main/java/com/xgame/common/viewmodels/ProfileViewModel.java
+++ xGame/Java/xgame/src/main/java/com/xgame/common/viewmodels/ProfileViewModel.java
@@ -1,5 +1,36 @@
 package com.xgame.common.viewmodels;
 
-public class ProfileViewModel {
+import java.util.List;
 
+import com.xgame.data.entities.ChessMatch;
+import com.xgame.data.entities.User;
+
+
+
+public class ProfileViewModel {
+	private User user;
+	private List<MatchViewModel> matchHistory;
+	
+	public ProfileViewModel(User user) {
+		this.setUser(user);
+		for(ChessMatch match : user.getBlackMatches()) {
+			matchHistory.add(new MatchViewModel(match));
+		}
+		
+		for(ChessMatch match : user.getWhiteMatches()) {
+			matchHistory.add(new MatchViewModel(match));
+		}
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+	public List<MatchViewModel> getMatchHistory(){
+		return matchHistory;
+	}
 }
