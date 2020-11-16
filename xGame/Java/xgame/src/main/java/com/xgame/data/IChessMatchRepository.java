765bfdb8465218f04a90a40843a406d54c4dcf45
wstnmssr <38819883+wstnmssr@users.noreply.github.com>
2020-11-16 15:34:29 -0700

    Update IChessMatchRepository.java
    get match by just status

--- xGame/Java/xgame/src/main/java/com/xgame/data/IChessMatchRepository.java
+++ xGame/Java/xgame/src/main/java/com/xgame/data/IChessMatchRepository.java
@@ -9,4 +9,5 @@ import com.xgame.data.entities.ChessMatch;
 
 public interface IChessMatchRepository extends JpaRepository<ChessMatch, Integer> {
 	List<ChessMatch> findByBlackPlayerIdAndMatchStatus(Integer id, MatchStatus matchStatus);
+	List<ChessMatch> findByMatchStatus(MatchStatus matchStatus);
 }
