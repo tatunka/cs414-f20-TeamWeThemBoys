6ebaacdcfddd1a80b0ed9c904b684b7ed5395482
Lance <lance.mcmullin@gmail.com>
2020-11-19 15:41:33 -0700

    Loads activeMatch into boardstate

--- xGame/client/src/Components/Match/Match.js
+++ xGame/client/src/Components/Match/Match.js
@@ -1,7 +1,9 @@
-import React from "react";
-import { Collapse, Row } from "reactstrap";
+import React, { useEffect, useState } from "react";
+import { Collapse, Row, Col } from "reactstrap";
 import PropTypes from "prop-types";
 import { Button } from "@material-ui/core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChess } from "@fortawesome/free-solid-svg-icons";
 
 import MatchPlayerStats from "./MatchPlayerStats";
 import MatchBoard from "./MatchBoard";
@@ -11,41 +13,57 @@ import "./MatchStyle.css";
 const Match = (props) => {
   const { isOpen, activeMatch } = props;
 
-  //add functionality to parse chessBoard into boardState
-  let boardState = [
-    { pieceName: "\u2659", location: "d1" },
-    { pieceName: "\u2659", location: "e2" },
-    { pieceName: "\u2659", location: "f2" },
-    { pieceName: "\u2659", location: "f3" },
-    { pieceName: "\u2659", location: "g4" },
-    { pieceName: "\u2659", location: "g3" },
-    { pieceName: "\u2659", location: "h5" },
-    { pieceName: "\u2659", location: "e4" },
-    { pieceName: "\u2656", location: "e1" },
-    { pieceName: "\u2656", location: "h4" },
-    { pieceName: "\u2657", location: "f1" },
-    { pieceName: "\u2657", location: "h2" },
-    { pieceName: "\u2658", location: "g1" },
-    { pieceName: "\u2658", location: "h3" },
-    { pieceName: "\u2655", location: "g2" },
-    { pieceName: "\u2654", location: "h1" },
-    { pieceName: "\u265F", location: "a4" },
-    { pieceName: "\u265F", location: "b5" },
-    { pieceName: "\u265F", location: "b6" },
-    { pieceName: "\u265F", location: "c6" },
-    { pieceName: "\u265F", location: "c7" },
-    { pieceName: "\u265F", location: "d5" },
-    { pieceName: "\u265F", location: "d7" },
-    { pieceName: "\u265F", location: "e8" },
-    { pieceName: "\u265C", location: "d8" },
-    { pieceName: "\u265C", location: "a5" },
-    { pieceName: "\u265D", location: "a7" },
-    { pieceName: "\u265D", location: "c8" },
-    { pieceName: "\u265E", location: "a6" },
-    { pieceName: "\u265E", location: "b8" },
-    { pieceName: "\u265B", location: "b7" },
-    { pieceName: "\u265A", location: "a8" }
-  ];
+  let boardStateDefault = [];
+
+  const [boardState, setBoardState] = useState(boardStateDefault);
+
+  useEffect(() => {
+    if (
+      activeMatch?.chessBoard &&
+      activeMatch.chessBoard !== "This is a chessboard"
+    ) {
+      const newBoard = parseBoard();
+      console.log("SETTING BOARD STATE", newBoard);
+      setBoardState(newBoard);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [activeMatch]);
+
+  const parseBoard = () => {
+    const newBoardState = [];
+    activeMatch.chessBoard.forEach((row) => {
+      row.forEach((piece) => {
+        if (piece) {
+          const newPiece = {
+            pieceName: getPieceName(piece.color, piece.type),
+            location: piece.position
+          };
+          newBoardState.push(newPiece);
+        }
+      });
+    });
+    return newBoardState;
+  };
+
+  const getPieceName = (color, type) => {
+    const isWhite = color === "WHITE";
+    switch (type) {
+      case "PAWN":
+        return isWhite ? "\u2659" : "\u265F";
+      case "ROOK":
+        return isWhite ? "\u2656" : "\u265C";
+      case "BISHOP":
+        return isWhite ? "\u2657" : "\u265D";
+      case "KNIGHT":
+        return isWhite ? "\u2658" : "\u265E";
+      case "KING":
+        return isWhite ? "\u2654" : "\u265A";
+      case "QUEEN":
+        return isWhite ? "\u2655" : "\u265B";
+      default:
+        break;
+    }
+  };
 
   const determineActiveColor = () => {
     return activeMatch?.turnCount % 2 === 0 ? "white" : "black";
@@ -54,22 +72,29 @@ const Match = (props) => {
   return (
     <Collapse isOpen={isOpen}>
       <Row>
-        <div className={"fullSize"}>
-          <MatchPlayerStats
-            playerName={activeMatch?.whitePlayerNickname}
-            activeColor={determineActiveColor()}
-            turnCounter={activeMatch?.turnCount}
-            boardState={boardState}
-          />
-          <MatchBoard boardState={boardState} activeColor={"white"} />
-          <MatchPlayerStats
-            playerName={activeMatch?.blackPlayerNickname}
-            activeColor={determineActiveColor()}
-            turnCounter={activeMatch?.turnCount}
-            boardState={boardState}
-            opponent={true}
-          />
-        </div>
+        {activeMatch ? (
+          <div className={"fullSize"}>
+            <MatchPlayerStats
+              playerName={activeMatch?.whitePlayerNickname}
+              activeColor={determineActiveColor()}
+              turnCounter={activeMatch?.turnCount}
+              boardState={boardState}
+            />
+            <MatchBoard boardState={boardState} activeColor={"white"} />
+            <MatchPlayerStats
+              playerName={activeMatch?.blackPlayerNickname}
+              activeColor={determineActiveColor()}
+              turnCounter={activeMatch?.turnCount}
+              boardState={boardState}
+              opponent={true}
+            />
+          </div>
+        ) : (
+          <Col className="pt-3 pb-3">
+            <FontAwesomeIcon icon={faChess} size="9x" />
+            <div className="h3 pt-3">No match open</div>
+          </Col>
+        )}
       </Row>
       <Row>
         <div className={"center"}>
@@ -84,13 +109,14 @@ const Match = (props) => {
 
 Match.propTypes = {
   isOpen: PropTypes.bool,
-  activeMatch: PropTypes.objectOf({
+  activeMatch: PropTypes.shape({
     id: PropTypes.number,
     whitePlayerId: PropTypes.number,
     blackPlayerId: PropTypes.number,
     whitePlayerNickname: PropTypes.string,
     blackPlayerNickname: PropTypes.string,
-    turnCount: PropTypes.number
+    turnCount: PropTypes.number,
+    chessBoard: PropTypes.array
   })
 };
 
@@ -102,7 +128,8 @@ Match.defaultProps = {
     blackPlayerId: 0,
     whitePlayerNickname: "",
     blackPlayerNickname: "",
-    turnCount: 0
+    turnCount: 0,
+    chessBoard: []
   }
 };
 
