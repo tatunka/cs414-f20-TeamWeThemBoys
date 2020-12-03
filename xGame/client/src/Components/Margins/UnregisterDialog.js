f03a0af139e1cf1606d60109bddd56809659a743
jesse-schmidt <Eterna1F1ames!!!>
2020-12-02 19:49:54 -0700

    add deregister call

--- xGame/client/src/Components/Margins/UnregisterDialog.js
+++ xGame/client/src/Components/Margins/UnregisterDialog.js
@@ -1,38 +1,76 @@
-import { Button, Dialog, DialogContent, DialogContentText, DialogTitle } from '@material-ui/core';
-import React from "react";
+import {
+  Button,
+  Dialog,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from "@material-ui/core";
+import React, { useEffect, useState } from "react";
 import { Row, Col } from "reactstrap";
 import PropTypes from "prop-types";
 
-const UnregisterDialog = (props) => {
-    const {
-        showUnregisterConfirmation,
-        setShowUnregisterConfirmation,
-    } = props;
+import * as userService from "../../service/userService";
 
-    return (
-        <Dialog open={showUnregisterConfirmation} onClose={() => {setShowUnregisterConfirmation(false)}} aria-labelledby="form-dialog-title">
-            <DialogTitle id="form-dialog-title">Warning</DialogTitle>
-            <DialogContent>
-                <DialogContentText>
-                    Are you sure you would like to unregister your account?
-                </DialogContentText>
-                <Row>
-                  <Col className="d-flex justify-content-center">
-                    <Button
-                      color="secondary"
-                      variant={"contained"}
-                      onClick={() => {}} //Not connected to backend yet
-                    >
-                        Confirm Deregistration
-                    </Button>
-                  </Col>
-                  <Col className="d-flex justify-content-center">
-                    <Button onClick={() => {setShowUnregisterConfirmation(false)}}>cancel</Button>
-                  </Col>
-                </Row>
-            </DialogContent>
-        </Dialog>
-    );
+const UnregisterDialog = (props) => {
+  const {
+    showUnregisterConfirmation,
+    setShowUnregisterConfirmation,
+    activeUser,
+    logoutUser,
+  } = props;
+  const [sendRequest, setSendRequest] = useState(false);
+
+  const deactivateRequest = async () => {
+    return await userService.deregister(activeUser.id);
+  };
+
+  useEffect(() => {
+    if (sendRequest) {
+      const deactivate = deactivateRequest();
+      if (deactivate) {
+        logoutUser();
+      }
+    }
+  }, [sendRequest]);
+
+  return (
+    <Dialog
+      open={showUnregisterConfirmation}
+      onClose={() => {
+        setShowUnregisterConfirmation(false);
+      }}
+      aria-labelledby="form-dialog-title"
+    >
+      <DialogTitle id="form-dialog-title">Warning</DialogTitle>
+      <DialogContent>
+        <DialogContentText>
+          Are you sure you would like to unregister your account?
+        </DialogContentText>
+        <Row>
+          <Col className="d-flex justify-content-center">
+            <Button
+              color="secondary"
+              variant={"contained"}
+              onClick={() => {
+                setSendRequest(true);
+              }}
+            >
+              Confirm Deregistration
+            </Button>
+          </Col>
+          <Col className="d-flex justify-content-center">
+            <Button
+              onClick={() => {
+                setShowUnregisterConfirmation(false);
+              }}
+            >
+              cancel
+            </Button>
+          </Col>
+        </Row>
+      </DialogContent>
+    </Dialog>
+  );
 };
 
 UnregisterDialog.propTypes = {
