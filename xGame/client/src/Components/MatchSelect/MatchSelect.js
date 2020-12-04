75a0f2e5f0cf7d73c18b18e342654fefbc41d4c1
aguentchev <aguentchev@users.noreply.github.com>
2020-12-04 03:28:30 -0700

    Update MatchSelect.js

--- xGame/client/src/Components/MatchSelect/MatchSelect.js
+++ xGame/client/src/Components/MatchSelect/MatchSelect.js
@@ -1,18 +1,94 @@
 import React from "react";
 import { Collapse } from "reactstrap";
 import PropTypes from "prop-types";
-import { Button } from '@material-ui/core';
+import { Button, Paper, Container } from '@material-ui/core';
+import { List, ListItem, ListItemSecondaryAction, ListItemText } from '@material-ui/core';
 import MatchCreateDialog from './MatchCreateDialog';
+import IconButton from '@material-ui/core/IconButton';
+import PlayIcon from '@material-ui/icons/PlayCircleOutline';
+import { useEffect, useState } from "react";
+import { Alert } from "@material-ui/lab";
+import { Formik } from "formik";
+
+import * as matchService from "../../service/matchService";
 
 const MatchSelect = (props) => {
-  const { isOpen, activeUser } = props;
+  const { isOpen, activeUser, setActiveMatch } = props;
   const [showMatchCreation, setShowMatchCreation] = React.useState(false);
+  const [selectError, setSelectError] = useState("");
+  var [matchList, setMatchList] = useState([]);
 
+  useEffect(() => {
+    async function fetchData() {
+      setMatchList(await matchService.getActiveMatches(activeUser?.id));
+      if (matchList?.error) setSelectError(matchList?.message);
+    }
+    fetchData();
+  }, [activeUser]);
 
   return (
     <Collapse isOpen={isOpen}>
       <div>
-        <p>add match selection data</p>
+        <br />
+        {selectError && (
+          <Alert
+            onClose={() => {
+              setSelectError("");
+            }}
+            severity="error"
+          >
+            {selectError}
+          </Alert>
+        )}
+        <Container maxWidth="sm">
+          <Paper elevation={0} style={{maxHeight: 200, overflow: 'auto'}}>
+
+          <Formik
+            onSubmit={async (values) => {
+              if(values) {
+                const match = await matchService.getMatch(values?.selectedMatch)
+                if (match?.error) setSelectError(match?.message);
+                else setActiveMatch(match);
+              }
+            }}
+            initialValues={{
+              selectedMatch: "",
+            }}
+          >
+            {(formProps) => {
+                const { setFieldValue, values } = formProps;
+                return (
+                  <List dense={true}>
+                    {matchList?.map((X) => {
+                      return (
+                        <ListItem key={X?.id}>
+                          <ListItemText
+                            primary={`Player ${X?.whitePlayerNickname} vs Player ${X?.blackPlayerNickname}`}
+                            secondary={`Match ID is ${X?.id}`}
+                          />
+                          <ListItemSecondaryAction>
+                            <IconButton
+                              type="submit"
+                              onClick={(e) => setFieldValue("selectedMatch", e.target.key)}
+                              edge="end"
+                              aria-label="play"
+                            >
+                              <PlayIcon />
+                            </IconButton>
+                          </ListItemSecondaryAction>
+                        </ListItem>
+                      );                  
+                    })}
+                  </List>
+                );
+            }}
+          </Formik>
+
+          </Paper>
+        </Container>
+
+
+
         <Button variant="outlined" color="primary" onClick={() => {setShowMatchCreation(true)} }>
                 Create a Match
         </Button>
@@ -26,12 +102,14 @@ const MatchSelect = (props) => {
 
 MatchSelect.propTypes = {
   activeUser: PropTypes.object,
-  isOpen: PropTypes.bool
+  isOpen: PropTypes.bool,
+  setActiveMatch: PropTypes.func
 };
 
 MatchSelect.defaultProps = {
   activeUser: {},
-  isOpen: true
+  isOpen: true,
+  setActiveMatch: () => {}
 };
 
 export default MatchSelect;
